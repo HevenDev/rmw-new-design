43d6ac97c1812c5e6b5d52c9b01a07dc25120827
HevenDev <hevendevofficial@gmail.com>
2025-04-08 12:30:49 +0530

    404 page updated

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for doesn't exist.</p>
+      <p>This page is not exists.</p>
     </div>
   );
 }
